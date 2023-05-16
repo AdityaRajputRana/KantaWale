65479e422eadc905a941b5fc4ccd7471447cad0e
Aditya Rana <adityarajputrana2016@gmail.com>
2023-05-16 22:25:39 +0530

    PhoneAuth - Login is Being implemented
    Upnext: OTP Layout

---
+++ app/src/main/java/com/guru/kantewala/Helpers/PhoneAuthHelper.java
@@ -0,0 +1,140 @@
+package com.guru.kantewala.Helpers;
+
+import android.app.Activity;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class PhoneAuthHelper {
+
+    public PhoneAuthHelper(PhoneAuthListener listener, Activity context) {
+        setUpPhoneAuth();
+        this.listener = listener;
+        this.context = context;
+    }
+
+    public PhoneAuthListener listener;
+    public Activity context;
+    //Todo: Define Enum for Error Codes
+    public interface PhoneAuthListener{
+        void authMessage(boolean success, String message, int code);
+    }
+
+    public void resendOTP(){
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(mPhoneNumber)
+                        .setForceResendingToken(mResendToken)
+                        .setTimeout(60L, TimeUnit.SECONDS)
+                        .setActivity(context)
+                        .setCallbacks(mCallbacks)
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+    private void authMessage(boolean success, String message, int code){
+        /*
+            Code Meanings for UI Changes or Logs
+            0 - Unknown Error: Check String message
+            1 - Phone Number is verified -
+            2 - "Invalid Phone number", -
+            3 - SMS Quota
+            4 - reCaptcha Failed
+            5 - OTP sent -
+            6 - Wrong OTP -
+            7 - Sign in failure: Check String message
+         */
+
+        if (listener != null && listener instanceof PhoneAuthListener){
+            listener.authMessage(success, message, code);
+        }
+        Log.i("PhoneAuthLogs" + code, message);
+    }
+
+    FirebaseAuth mAuth;
+    String mVerificationId;
+    String mPhoneNumber;
+    PhoneAuthProvider.ForceResendingToken mResendToken;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    private void setUpPhoneAuth(){
+        mAuth = FirebaseAuth.getInstance();
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential credential) {
+                authMessage(true, "Phone Number verified", 1);
+                signInWithPhoneAuthCredential(credential);
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                    authMessage(false, "Invalid Phone number", 2);
+                } else if (e instanceof FirebaseTooManyRequestsException) {
+                    authMessage(false, "SMS Quota Exceeded", 3);
+                } else if (e instanceof FirebaseAuthMissingActivityForRecaptchaException) {
+                    authMessage(false, "reCaptcha Failed", 4);
+                } else {
+                    authMessage(false, e.getMessage(), 0);
+                }
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId,
+                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                authMessage(true, "OTP sent", 5);
+                mVerificationId = verificationId;
+                mResendToken = token;
+            }
+        };
+    }
+    public void sendOTP(String phoneNumber){
+        mPhoneNumber = phoneNumber;
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNumber)
+                        .setTimeout(60L, TimeUnit.SECONDS)
+                        .setActivity(context)                 // (optional) Activity for callback binding
+                        // If no activity is passed, reCAPTCHA verification can not be used.
+                        .setCallbacks(mCallbacks)
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+    public void verifyOTP(String otp){
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, otp);
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(context, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            authMessage(true, "Sign In Successful!", 10);
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                authMessage(false, "The otp entered was incorrect", 6);
+                            } else {
+                                authMessage(false, task.getException().getMessage(), 7);
+                            }
+                        }
+                    }
+                });
+    }
+}
--- app/src/main/java/com/guru/kantewala/LoginActivity.java
+++ app/src/main/java/com/guru/kantewala/LoginActivity.java
@@ -2,13 +2,158 @@ package com.guru.kantewala;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+import com.guru.kantewala.Helpers.PhoneAuthHelper;
+import com.guru.kantewala.databinding.ActivityLoginBinding;
+import com.guru.kantewala.databinding.DialogLoadingBinding;
+
+public class LoginActivity extends AppCompatActivity implements PhoneAuthHelper.PhoneAuthListener {
+
+    //Todo: Make the activity render after pause using a full state class - less priority
+
+    //Todo: Implement country code picker
+    int requiredPhoneNumberDigits = 10;
+    String countryCode = "+91";
+    
+    ActivityLoginBinding binding;
+    PhoneAuthHelper helper;
+    AlertDialog dialog;
+    DialogLoadingBinding dialogBinding;
+    
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        helper = new PhoneAuthHelper(this, this);
+        setListeners();
+    }
+
+
+
+    private void setListeners() {
+        binding.continueBtn.setOnClickListener(view->{
+            if (validPhoneInput()){
+                String phoneNumber = countryCode + binding.phoneNumberEt.getText().toString();
+                startProgress("We are sending OTP to your mobile number");
+                helper.sendOTP(phoneNumber);
+            }
+
+        });
+
+        binding.registerLayout.setOnClickListener(view->{
+            Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+            LoginActivity.this.startActivity(i);
+        });
+    }
+
+    private void startProgress(String message) {
+        if (dialogBinding == null){
+            dialogBinding = DialogLoadingBinding.inflate(getLayoutInflater(), null, false);
+        }
+
+        dialogBinding.imageView.setVisibility(View.GONE);
+        dialogBinding.progressBar.setVisibility(View.VISIBLE);
+        dialogBinding.titleTxt.setText("Please wait");
+        dialogBinding.bodyTxt.setText(message);
+
+        if (dialog == null){
+            dialog = new android.app.AlertDialog.Builder(this)
+                    .setView(dialogBinding.getRoot())
+                    .setOnDismissListener(d->{
+                        dialogBinding = null;
+                        dialog = null;
+                    })
+                    .show();
+
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+        dialog.setCancelable(false);
+    }
+
+    private boolean validPhoneInput() {
+        boolean isValidInput = true;
+        String error = "";
+        
+        if (binding.phoneNumberEt.getText().toString().length() != requiredPhoneNumberDigits){
+            error = "Please enter a valid " + requiredPhoneNumberDigits + " digit mobile number";
+            isValidInput = false;
+        }
+        
+        if (error.isEmpty())
+            error = null;
+        binding.phoneNumberEt.setError(error);
+        return isValidInput;
+    }
+
+
+    private void showError(String message){
+        if (dialogBinding == null){
+            dialogBinding = DialogLoadingBinding.inflate(getLayoutInflater(), null, false);
+        }
+
+        dialogBinding.imageView.setVisibility(View.VISIBLE);
+        dialogBinding.imageView.setImageDrawable(getDrawable(R.drawable.ic_error_bg));
+        dialogBinding.progressBar.setVisibility(View.GONE);
+        dialogBinding.titleTxt.setText("Some Error Occurred");
+        dialogBinding.bodyTxt.setText(message);
+
+        if (dialog == null) {
+            dialog = new AlertDialog.Builder(this)
+                    .setView(dialogBinding.getRoot())
+                    .setOnDismissListener(d->{
+                        dialogBinding = null;
+                        dialog = null;
+                    })
+                    .show();
+
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+
+        dialog.setCancelable(true);
+    }
+
+
+    @Override
+    public void authMessage(boolean success, String message, int code) {
+        switch (code){
+            case 5:
+                showOTPLayout();
+                break;
+            case 2:
+                invalidPhoneNumber();
+                break;
+            default:
+                showError(message);
+                break;
+        }
+    }
+
+    private void dismissProgressDialog(){
+        if (dialog != null){
+            dialog.dismiss();
+            dialog = null;
+            dialogBinding = null;
+        }
+    }
+
+    private void invalidPhoneNumber() {
+        dismissProgressDialog();
+        binding.phoneNumberEt.setError("This phone number is not valid");
+    }
+
+
+    private void showOTPLayout() {
+        dismissProgressDialog();
     }
 }
